920c028380f5cbb664d855736614e1541aab80ea
richardss <rolandka88@gmail.com>
2018-06-13 10:09:49 +0200

    Add files via upload

---
+++ incremental.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+function usage {
+        printf "Usage: $(basename $0) -p PARAMETER \n"
+        exit 1
+}
+REPO='(REPO_CRE_TGET_I, REPO_CRE_TGET_C)'
+DIR='$(/prod/dati/public/B3/T7/"$AZ"/"$DATA"/"${REPO[@]}"/"${REPO[@]}".*)'
+DATA=20${PARAMETER:0:6}
+AZ=${PARAMETER:6:5}
+VAR_START=50
+VAR_OPS=51
+
+while getopts :p: opts; do
+case "$opts" in
+   p)
+        PARAMETER=$OPTARG ;;
+   *)
+        printf "ERR | Missing mandatory parameters\n"
+        usage ;;
+esac
+done
+if [ -z $PARAMETER ] ; then
+    usage
+fi
+if [[ "$AZ" == "UUD01" && $ "$REPO" == "REPO_CRE_TGET_C" ]]; then
+PROG=$((VAR_START+1))
+else
+VAR_START=$VAR_OPS
+PROG=$((VAR_START+1))
+fi
+for file in $(DIR);
+do  mv "${file}" /prod/dati/public/B3/$AZ/$DATA/_B3/"${REPO[@]}"/"${REPO[@]}"."${PROG}".txt
+PROG=$((VAR_START+1))
+fi
+done
