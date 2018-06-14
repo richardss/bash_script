9918b26ccb88072b6c57fd986d7e4dad6b4e3d4b
richardss <rolandka88@gmail.com>
2018-06-14 15:16:14 +0200

    Update incremental.sh

--- incremental.sh
+++ incremental.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/riccardo/incremental/log/archives/
-n=$(cat /home/riccardo/incremental/lotti.txt)
+cd /home/dir
+n=$(cat /home/dir/file.txt)
 d=$((n+1))
-for file in REPO_CRE_TGET_I.*;
-do  mv  "${file}" /home/riccardo/incremental/log/archives1/REPO_CRE_TGET_I."${d}".txt
+for file in name.*;
+do  mv  "${file}" /home/dir/incremental/log/archives/name."${d}".txt
 d=$((d+1));
 done
 
